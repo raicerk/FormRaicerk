2e3728476bcc3640a2dfb42b3a287331066a3940
Juan Mora <jvmora@raicerk.cl>
2014-09-13 20:46:54 -0400

    Se modifica archivo para uso como plugins

--- captcha.php
+++ captcha.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+
+include_once('../../../wp-load.php');
+
 class captcha{
     private function creacapta(){
 
@@ -7,23 +10,58 @@ class captcha{
         $_SESSION['IdRespuesta'] = $opcion;
         switch($opcion){
             case '1':
-                return "<input id='txtRespuesta' name='txtRespuesta' value='' placeholder='' style='width: 20px;'> - 2 = 4";
+                return "<input id='txtRespuesta' name='txtRespuesta' value='' placeholder='' style='width: 20px; padding: 0;'> - 2 = 4";
             break;
             case '2':
-                return "<input id='txtRespuesta' name='txtRespuesta' value='' placeholder='' style='width: 20px;'> + 2 = tres";
+                return "<input id='txtRespuesta' name='txtRespuesta' value='' placeholder='' style='width: 20px; padding: 0;'> + 2 = tres";
             break;
             case '3':
-                return "<input id='txtRespuesta' name='txtRespuesta' value='' placeholder='' style='width: 20px;'> - uno = 2";
+                return "<input id='txtRespuesta' name='txtRespuesta' value='' placeholder='' style='width: 20px; padding: 0;'> - uno = 2";
             break;
             case '4':
-                return "<input id='txtRespuesta' name='txtRespuesta' value='' placeholder='' style='width: 20px;'> * 2 = 6";
+                return "<input id='txtRespuesta' name='txtRespuesta' value='' placeholder='' style='width: 20px; padding: 0;'> * 2 = 6";
             break;
             case '5':
-                return "<input id='txtRespuesta' name='txtRespuesta' value='' placeholder='' style='width: 20px;'> * 1 = 5";
+                return "<input id='txtRespuesta' name='txtRespuesta' value='' placeholder='' style='width: 20px; padding: 0;'> * 1 = 5";
             break;
         }
     }
-
+    private function mandamail(){
+        $to = array('raicerk@gmail.com','raicerk@outlook.com');
+        $subject = "Prueba de correo desde wordpress";
+        $headers = 'Reply-to: Juan Mora <jvmora@raicerk.cl>' . "\r\n";
+        $message.='Hola <br/>';
+        $message.='Esto es una prueba de wordpress';
+        add_filter('wp_mail_content_type',create_function('', 'return "text/html";'));
+        add_filter('wp_mail_from',create_function('','return "callcenter@raicerk.cl";'));
+        add_filter('wp_mail_from_name',create_function('','return "Wordpress";'));
+        wp_mail( $to, $subject, $message, $headers);
+    }
+    public function opcionesmenu(){
+        $html = "";
+        $html .= "<table>";
+        $html .= "<tr>";
+        $html .= "<td><label>Correo Destinatario</label></td>";
+        $html .= "<td><input id='' name='' class='' type='text'></td>";
+        $html .= "</tr>";
+        $html .= "<td><label>Correo Copia</label></td>";
+        $html .= "<td><input id='' name='' class='' type='text'></td>";
+        $html .= "</tr>";
+        $html .= "<td><label>Nombre Salida</label></td>";
+        $html .= "<td><input id='' name='' class='' type='text'></td>";
+        $html .= "</tr>";
+        $html .= "<td><label>Nombre responder a</label></td>";
+        $html .= "<td><input id='' name='' class='' type='text'></td>";
+        $html .= "</tr>";
+        $html .= "<td><label>Responder a Correo</label></td>";
+        $html .= "<td><input id='' name='' class='' type='text'></td>";
+        $html .= "</tr>";
+        $html .= "<td><!-- --></td>";
+        $html .= "<td><input id='' name='' class='' type='button' value='Guardar'></td>";
+        $html .= "</tr>";
+        $html .= "</table>";
+        return $html;
+    }
     public function formularioCaptcha($pagina){
 
         if($_POST){
@@ -38,11 +76,13 @@ class captcha{
             if($respuesta[$_SESSION['IdRespuesta']] != $_POST['txtRespuesta']){
                 $estado = "Error";
             }else{
+                $this->mandamail();
                 $estado = "Exito";
             }
         }
-
-        $html = "<form action='".$pagina."' method='POST'>";
+        $html = "";
+        $html .= "<div id='contactoraicerk'>";
+        $html .= "<form action='".$pagina."' method='POST'>";
         $html .= "<table>";
         $html .= "<tr>";
         $html .= "<td><label>Nombre</label></td>";
@@ -72,11 +112,21 @@ class captcha{
         $html .= "</tr>";
         $html .= "</table>";
         $html .= "</form>";
+        $html .= "</div>";
         return $html;
     }
-
-
 }
+?>
+
+<?php
+
+
+
+
+
+
+
+
 ?>
 
 
