0bb289ebf54f4fcaa3d5dfe99257646af4c60279
Juan Mora <jvmora@raicerk.cl>
2014-09-13 20:47:57 -0400

    Se modifica principal, para formato de wordpress

--- formraicerk.php
+++ formraicerk.php
@@ -1,7 +1,33 @@
 <?php
-session_start();
-include 'captcha.php';
-$captcha = new captcha();
-echo $captcha->formularioCaptcha("index.php");
+/*
+Plugin Name: Form de Raicerk
+Plugin URI: http://raicerk.cl/wordpress
+Description: Formulario de envio de correos y validacion de spam
+Version: 1.0
+Author: Juan Mora
+Author URI: http://facebook.com/Raicerk
+License: GPL2
+*/
 
+
+session_start();
+include_once('captcha.php');
+$captcha = new captcha();
+add_action('wp_footer', 'instancia');
+add_action('admin_menu', 'PanelOpcionesFormRaicerk');
+
+
+function PanelOpcionesFormRaicerk(){
+    add_menu_page('Administrador de Form Raicerk','FormRaicerk','administrator','FormRaicerk','opciones_FormRaicerk');
+}
+
+function opciones_FormRaicerk(){
+    $captcha = new captcha();
+    echo $captcha->opcionesmenu();
+}
+
+function instancia(){
+    $captcha = new captcha();
+    echo $captcha->formularioCaptcha("index.php");
+}
 ?>
