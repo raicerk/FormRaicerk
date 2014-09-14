ba787ac9bc665d1512f5da8204256f1e4c0ed88c
Juan Mora <jvmora@raicerk.cl>
2014-09-13 22:46:37 -0400

    Se reparan errores de activacion de plugins

--- formraicerk.php
+++ formraicerk.php
@@ -9,17 +9,19 @@ Author URI: http://facebook.com/Raicerk
 License: GPL2
 */
 session_start();
-include_once('captcha.php');
+
 add_action('wp_footer', 'instancia');
 add_action('admin_menu', 'PanelOpcionesFormRaicerk');
 function PanelOpcionesFormRaicerk(){
     add_menu_page('Administrador de Form Raicerk','FormRaicerk','administrator','FormRaicerk','opciones_FormRaicerk');
 }
 function opciones_FormRaicerk(){
+    include_once('captcha.php');
     $captcha = new captcha();
     echo $captcha->opcionesmenu();
 }
 function instancia(){
+    include_once('captcha.php');
     $captcha = new captcha();
     echo $captcha->formularioCaptcha("index.php");
 }
